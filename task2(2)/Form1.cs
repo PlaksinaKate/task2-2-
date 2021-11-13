a3d7cea71e5a65b7b27eb9566af456099210dcfa
eplaskina <67640529+PlaksinaKate@users.noreply.github.com>
2021-11-14 00:35:33 +0300

    Добавьте файлы проекта.

---
+++ task2(2)/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2_2_
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((myStream = openFileDialog1.OpenFile()) != null)
+                {
+                    string strfilename = openFileDialog1.FileName;
+                    string filetext = File.ReadAllText(strfilename);
+                    richTextBox1.Text = filetext;
+                    string[] text = filetext.Split('\n');
+                    
+                    string txt = string.Join("\n", Main.wordCheck(text).ToArray());
+                    richTextBox2.Text = txt;
+
+                }
+            }
+
+            
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+            saveFile1.DefaultExt = "*.txt";
+            saveFile1.Filter = "Text files|*.txt";
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+                saveFile1.FileName.Length > 0)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFile1.FileName, true))
+                {
+                    sw.WriteLine(richTextBox2.Text);
+                    sw.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ task2(2)/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace task2_2_
+{
+    internal class Main
+    {
+
+
+        public static string[] readFile(string fileName)
+        {
+            string[] lines = null;
+            try
+            {
+                lines = System.IO.File.ReadAllLines(fileName/*@"../../input.txt"*/);
+                Console.WriteLine("Текст:");
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                return lines;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+
+            return lines;
+        }
+
+        public static List<string> wordCheck(string[] text)
+        {
+            string pattern = @"(\b[С]\w+|\w+[С]\b)";
+            Regex rg = new Regex(pattern, RegexOptions.IgnoreCase);
+            List<string> correctWords = new List<string>();
+
+            Console.WriteLine("");
+            Console.WriteLine("Найденные слова:");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                MatchCollection matchedAuthors = rg.Matches(text[i]);
+                for (int count = 0; count < matchedAuthors.Count; count++)
+                {
+                    Console.WriteLine(matchedAuthors[count].Value);
+                    correctWords.Add(matchedAuthors[count].Value);
+                }
+            }
+
+            return correctWords;
+        }
+
+        public static void writeInFile(List<string> correctWords)
+        {
+            try
+            {
+                using StreamWriter file = new(@"C:\Users\Ekaterina\RiderProjects\task2_3\task2_3\output.txt");
+
+                //Writeout the numbers 1 to 10 on the same line.
+                foreach (string word in correctWords)
+                {
+                    file.WriteLine(word);
+
+                }
+                //close the file
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+
+        }
+
+
+
+    }
+}
